8175c0aedfa50783fad9ab104deed17d96f53370
kylahenry <kylachenry15@gmail.com>
2024-11-26 00:04:26 -0500

    library: added bookshelf header w/ static data {will fill later} user profile: compleated header for page {add bottom & others later} {settings button on top page to be added}

--- InkyApp/Tabs/Library.swift
+++ InkyApp/Tabs/Library.swift
@@ -6,8 +6,53 @@
 import SwiftUI
 
 struct Library: View {
+//View Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical){
+            LazyVStack(spacing: 15){
+                DummyMessagesView()
+            }
+            .safeAreaPadding(15)
+            .safeAreaInset(edge: .top, spacing: 0){
+                LibraryTitle()
+            }
+        }
+    }
+    
+//Expandable Navigation Bar
+    @ViewBuilder
+    func LibraryTitle(_ title:String = "Bookshelf") -> some View {
+        VStack(spacing: 10){
+            //Title
+            Text(title)
+                .font(.largeTitle.bold())
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom, 10)
+        }
+        .padding(.top, 25)
+        .padding(.horizontal, 15)
+        .padding(.bottom, 10)
+    }
+    
+    
+// Dummy Messages View
+    @ViewBuilder
+    func DummyMessagesView() -> some View {        ForEach(0..<20, id: \.self){ _ in
+            HStack (spacing: 12){
+                Circle()
+                    .frame(width: 55, height: 55)
+                VStack(alignment: .leading, spacing: 6, content: {
+                    Rectangle()
+                        .frame(width: 140, height: 8)
+                    Rectangle()
+                        .frame(height: 8)
+                    Rectangle()
+                        .frame(width: 80, height: 8)
+                })
+            }
+            .foregroundStyle(.gray.opacity(0.4))
+            .padding(.horizontal, 15)
+        }
     }
 }
 
--- InkyApp/Tabs/UserProfile.swift
+++ InkyApp/Tabs/UserProfile.swift
@@ -8,30 +8,49 @@
 import SwiftUI
 
 struct UserProfile: View {
+    let gradient = Gradient(colors: [.inkblue, .inkwhite])
     var body: some View {
-        NavigationStack{
             VStack {
                 HStack {
-                    Image("person.crop.circle")
-                        .resizable()
-                    .frame(width: 60, height: 60)
-                    .overlay {
-                            Circle()
-                                .stroke(.gray.opacity(0.4),
-                                        lineWidth: 2)
-                                .frame(width: 90, height: 80)
-                        }
+                    Spacer()
+                    
+                    VStack {
+                        Image("demoprofilepic")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .clipped()
+                            .overlay(Circle().stroke(Color.white, lineWidth: 4 ))
+                            .padding(.top, 52)
+                        
+                        Text("Estella Bella").font(.system(size: 20, weight: .semibold)).foregroundColor(.white)
+                            .padding(.top, 10)
+                        
+                        Text("@Its.Estella").font(.system(size: 18)).foregroundColor(.white)
+                            .padding(.top, 2)
+                        
+                        Text("3").font(.system(size: 20, weight: .semibold)).foregroundColor(.white)
+                            .padding(.top, 4)
+                            .offset(x: -64, y: 10)
+                        
+                        Text("Creations").font(.system(size: 20)).foregroundColor(.white)
+                            .offset(x: -64, y: 12)
+
+                        Text("101.7k").font(.system(size: 20, weight: .semibold)).foregroundColor(.white)
+                            .padding(.top, 4)
+                            .offset(x: 64, y: -54)
+                        
+                        Text("Followers").font(.system(size: 20)).foregroundColor(.white)
+                            .offset(x: 64, y: -52)
+
+                    }
                     Spacer()
                 }
-                .padding([.leading, .trailing])
+                Spacer()
             }
-            .padding()
-            .toolbar {
-                ToolbarItem(placement: .principal){
-                  
-                }
-            }
-        }
+        .background(LinearGradient(gradient: gradient, startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all))
     }
 }
 
